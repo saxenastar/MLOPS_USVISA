9ff64a11d76a056c9e40abaa92c391b54ff9d2c1
saxenastar <saxenastar.123@gmail.com>
2024-10-09 18:02:34 +0530

    "read me and setup.py update"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Amit",
+    author_email="saxenastar.123@gmail.com",
     packages=find_packages()
 )
